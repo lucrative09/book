5f698413553b009b324ee9a61e55fd3389ac6552
0general <0general@naver.com>
2019-10-06 16:41:59 +0900

    MvcUser 커밋입니다.

---
+++ bookTeamProject/src/main/java/com/spring/book/mvcuser/domain/MvcUser.java
@@ -0,0 +1,33 @@
+package com.spring.book.mvcuser.domain;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class MvcUser {
+	
+	private String email;
+	private String password;
+	private String nickname;
+	private int phoneNumber;
+	private String bank;
+	private int bankAccount;
+	private String auth;
+	private double rate;
+	private String sessionId;
+	private Date limitTime;
+	private Date regDate;
+	private int businessNum;
+	private String buildingAddress;
+	private int userReserveReservePageId;
+
+}
